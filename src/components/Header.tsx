002cf70ea60a1b9e159faf1b72da16169337df51
Gabriel Palhares <gabrielpalharesdev@gmail.com>
2024-03-12 14:24:19 -0300

    fixing files relative paths

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export function Header() {
   return (
     <header className={styles.container}>
-      <img src="/logo.svg" alt="App logo" />
+      <img src="public\logo.svg" alt="App logo" />
     </header>
   )
 }
--- src/components/List/Empty.tsx
+++ src/components/List/Empty.tsx
@@ -3,7 +3,7 @@ import styles from './Empty.module.css'
 export function Empty() {
   return (
     <div className={styles.container}>
-      <img src="/clipboard.png" alt="ícone de prancheta" />
+      <img src="public\clipboard.png" alt="ícone de prancheta" />
       <p>
         <strong>You don't have any tasks registered yet</strong>
         Create tasks and organize your to-do items
