a81cc89f74df1ceb70fcc1d5b84106f9fc8a0a43
Gabriel Palhares <gabrielpalharesdev@gmail.com>
2024-03-12 14:35:21 -0300

    Updates

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Header.module.css'
-
-export function Header() {
-  return (
-    <header className={styles.container}>
-      <img src="public\logo.svg" alt="App logo" />
-    </header>
-  )
-}
